07650083dae54bb35405683236a4165b247f2c03
Hesham El-Sawaf <heshamlsawaf@gmail.com>
2017-11-28 20:42:47 +0200

    Fix pintos utils scripts & qemu shutdown sequence

--- utils/Pintos.pm
+++ utils/Pintos.pm
@@ -359,7 +359,7 @@ sub cyl_sectors {
 # Makes sure that the loader is a reasonable size.
 sub read_loader {
     my ($name) = @_;
-    $name = find_file ("/home/anas_harby/pintos/src/threads/build/loader.bin") if !defined $name;
+    $name = find_file ($ENV{"HOME"}."/os-pintos/pintos/src/threads/build/loader.bin") if !defined $name;
     die "Cannot find loader\n" if !defined $name;
 
     my ($handle);
@@ -489,3 +489,4 @@ sub max {
 }
 
 1;
+
